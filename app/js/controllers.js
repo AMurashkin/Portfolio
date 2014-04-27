643d6d8707e37dfd59e6befc40c53b17f74ae126
Alexander Murashkin <amurashk@gsd.uwaterloo.ca>
2014-04-27 07:08:56 -0400

    Implemented Bio and Background

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,26 @@ var projectcatControllers = angular.module('projectcatControllers', []);
 projectcatControllers.controller('projectListCtrl', ['$scope', 'project',
   function($scope, project) {
     $scope.projects = project.query();
+    $scope.user = {};
+    $scope.user.name = 'Alexandr Murashkin';
+    $scope.user.photoUrl = 'img/core/alexandr_murashkin.png';
+    $scope.user.linkedInUrl = 'http://www.linkedin.com/pub/alexander-murashkin/59/262/527';
+    $scope.user.labUrl = 'http://gsd.uwaterloo.ca/amurashk';
+    $scope.user.cvUrl = 'binary/alexandr_murashkin.pdf';
+    $scope.user.degree = [];
+    $scope.user.degree[0] = {
+      'title': 'MMath in Computer Science, University of Waterloo, ON, Canada (expected Aug 2014)',
+      'icon': 'img/core/ca.png'
+    };
+    $scope.user.degree[1] = {
+      'title': 'BSc in Computer Systems and Software, Kazakh-British Technical University, Kazakhstan (2012)',
+      'icon': 'img/core/kz.png'
+    };
+
+    $scope.user.status = 'Graduate Research Assistant, writing a Master\'s thesis';
+    $scope.user.phone = '+1 (226) 600 5529';
+    $scope.user.email = 'amurashk@gsd.uwaterloo.ca';
+    
     $scope.orderProp = 'age';
   }]);
 
