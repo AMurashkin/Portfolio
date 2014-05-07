e5d16354fc9754dafb63438128847697ae67ad13
AMurashkin <alexmur90@gmail.com>
2014-05-07 19:00:04 -0400

    Created a separate user.js file

---
+++ app/data/user.js
@@ -0,0 +1,25 @@
+function user()
+{
+    var user = {};
+    user.name = 'Alexandr Murashkin';
+    user.photoUrl = 'img/core/alexandr_murashkin.png';
+    user.linkedInUrl = 'http://www.linkedin.com/pub/alexander-murashkin/59/262/527';
+    user.labUrl = 'http://gsd.uwaterloo.ca/amurashk';
+    user.cvUrl = 'binary/alexandr_murashkin.pdf';
+    user.degree = [];
+    user.degree[0] = {
+      'title': 'MMath in Computer Science, University of Waterloo, ON, Canada (expected Aug 2014)',
+      'icon': 'img/core/ca.png'
+    };
+    user.degree[1] = {
+      'title': 'BSc in Computer Systems and Software Engineering, Kazakh-British Technical University, Kazakhstan (2012)',
+      'icon': 'img/core/kz.png'
+    };
+
+    user.status = 'Graduate Research Assistant, writing a Master\'s thesis';
+    user.phone = '+1 (226) 600 5529';
+    user.email = 'amurashk@gsd.uwaterloo.ca';
+    user.cv = 'binary/core/alexander_murashkin.pdf';
+
+    return user;
+}
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,27 +8,7 @@ projectcatControllers.controller('projectListCtrl', ['$scope',
   function($scope) {
     $scope.projects = ProjectList();
     //$scope.projects = project.query();
-    $scope.user = {};
-    $scope.user.name = 'Alexandr Murashkin';
-    $scope.user.photoUrl = 'img/core/alexandr_murashkin.png';
-    $scope.user.linkedInUrl = 'http://www.linkedin.com/pub/alexander-murashkin/59/262/527';
-    $scope.user.labUrl = 'http://gsd.uwaterloo.ca/amurashk';
-    $scope.user.cvUrl = 'binary/alexandr_murashkin.pdf';
-    $scope.user.degree = [];
-    $scope.user.degree[0] = {
-      'title': 'MMath in Computer Science, University of Waterloo, ON, Canada (expected Aug 2014)',
-      'icon': 'img/core/ca.png'
-    };
-    $scope.user.degree[1] = {
-      'title': 'BSc in Computer Systems and Software, Kazakh-British Technical University, Kazakhstan (2012)',
-      'icon': 'img/core/kz.png'
-    };
-
-    $scope.user.status = 'Graduate Research Assistant, writing a Master\'s thesis';
-    $scope.user.phone = '+1 (226) 600 5529';
-    $scope.user.email = 'amurashk@gsd.uwaterloo.ca';
-    $scope.user.cv = 'binary/core/alexander_murashkin.pdf';
-    
+    $scope.user = user();    
     $scope.orderProp = 'weight';
   }]);
 
